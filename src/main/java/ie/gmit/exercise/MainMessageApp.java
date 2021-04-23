5373c8c9b7196a5ae854809a22b55dff9f69de0a
Sean <g00358883@gmit.ie>
2021-04-23 11:12:32 +0100

    Refactor MessagePrinter and MainApp

--- src/main/java/ie/gmit/exercise/MainMessageApp.java
+++ src/main/java/ie/gmit/exercise/MainMessageApp.java
@@ -1,14 +1,20 @@
 package ie.gmit.exercise;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MainMessageApp {
 
 	public static void main(String[] args) throws IOException {
 
+		Formatter formatter = new TextFormatter();//creates formatter
+
+		PrintWriter writer = new PrintWriter(System.out);
+
 		Message msg = new Message("This is a message");
 		MessagePrinter printer = new MessagePrinter();
-		printer.writeMessage(msg, "test_msg.txt");
+		printer.writeMessage(msg, formatter,writer);
 	}
-
 }
+
